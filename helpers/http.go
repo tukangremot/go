a8a58463c479ef1e8e98cd417f1fbeacf3b97035
Irvan Maulana <me@tukangremot.com>
2021-08-16 08:25:54 +0700

    refactor

---
+++ helpers/http.go
@@ -0,0 +1,15 @@
+package helpers
+
+type HttpResponseBody struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func BuildHttpResponseBody(success bool, message string, data interface{}) HttpResponseBody {
+	return HttpResponseBody{
+		Success: success,
+		Message: message,
+		Data:    data,
+	}
+}
